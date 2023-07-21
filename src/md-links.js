a9c524bd6824a9f061f8675ec51964952f949960
Natalia392 <ntorrejonvalencia@gmail.com>
2023-07-21 16:22:55 -0400

    lee archivos md y los retorna array con rutas de archivos md. Luego extrae links

--- src/md-links.js
+++ src/md-links.js
@@ -4,28 +4,21 @@ import {
 } from './index.js';
 
 // Introducir en la terminal node ./src/index.js README.md para correr la app.
-const route = process.argv[2];
+// const route = process.argv[2];
 
-const mdLinks = (path /*,options*/ ) => new Promise((resolve, reject) => {
-  let myPath = path;
-  // Identifica si la ruta existe y si es relativa, la cambia a absoluta
-  if (routeExists(path)) {
-    myPath = toAbsolutePath(path);
-    resolve('La ruta existe');
-  } else {
-    // Si la ruta no existe se rechaza la promesa
+const mdLinks = (path /*,options*/ ) => {
+  return new Promise((resolve, reject) => {
+    let myPath = path;
+    // Identifica si la ruta existe y si es relativa, la cambia a absoluta
+    if (routeExists(path)) {
+      myPath = toAbsolutePath(path);
+      resolve('La ruta existe');
+    } else {
+      // Si la ruta no existe se rechaza la promesa
+    }
+    
     reject('La ruta no existe');
-  }
-});
-
-// ----ESTO LUEGO IRÁ EN CLI ---------------------
-mdLinks(route)
-  .then((rutaAbsoluta) => {
-    console.log('.then:', (rutaAbsoluta), -1);
-  })
-  .catch((error) => {
-    console.log('.catch: LA RUTA NO EXISTE', -2);
-    console.error((error));
   });
+};
 
 export default mdLinks;
